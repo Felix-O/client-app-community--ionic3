ea1182f96bd50f5bd1bbb4a21a696a27905cf726
Patrick Odum <felix.oje@gmail.com>
2018-01-10 16:06:54 -0800

    user page

--- src/pages/user/user.ts
+++ src/pages/user/user.ts
@@ -8,18 +8,28 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * Ionic pages and navigation.
  */
 
-@IonicPage()
+@IonicPage({
+  segment: 'user/:un'
+})
 @Component({
   selector: 'page-user',
   templateUrl: 'user.html',
 })
 export class UserPage {
 
+  userID: any;
+  firstname: any;
+  lastname: any;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad UserPage');
+    this.userID = this.navParams.get('uid');
+    this.firstname = this.navParams.get('fn');
+    this.lastname = this.navParams.get('ln');
+    //console.log(this.user);
   }
 
 }
