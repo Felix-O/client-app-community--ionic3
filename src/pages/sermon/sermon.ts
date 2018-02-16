af7a7b6cdcd221153f362068e06274ac8cd4407c
Patrick Odum <felix.oje@gmail.com>
2018-02-16 02:15:42 -0800

    sermon.ts: remap api data

--- src/pages/sermon/sermon.ts
+++ src/pages/sermon/sermon.ts
@@ -33,12 +33,12 @@ export class SermonPage {
     else{
       this.wpService.getSermon(sermonQuery).then( data => {
         console.log(data);/**/
-        this.content = data.content.rendered;
+        //this.content = data.content.rendered;
       });/**/
       let mediaQuery = "?parent=" + this.sermonId;
       this.wpService.getMedia(mediaQuery).then((mediaData) => {
-        console.log(mediaData.source_url);
-        this.media = mediaData;
+        //console.log(mediaData.source_url);
+        //this.media = mediaData;
       });
     }
   }
