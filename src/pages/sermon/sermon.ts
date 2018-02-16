3952991dd931ed75be6a58b999ebc56fb795ddc4
Patrick Odum <felix.oje@gmail.com>
2018-02-16 03:22:16 -0800

    sermon.ts: remap api data

--- src/pages/sermon/sermon.ts
+++ src/pages/sermon/sermon.ts
@@ -3,7 +3,11 @@ import { App, IonicPage, NavController, NavParams } from 'ionic-angular';
 import { WpProvider } from '../../providers/wp/wp';
 
 interface sermonType {
-      content: any
+    content: any
+}
+
+interface mediaType {
+    source_url: any
 }
 
 @IonicPage({
@@ -36,15 +40,15 @@ export class SermonPage {
       this.app.getRootNav().setRoot('SermonsPage');
     }
     else{
-      this.wpService.getSermon(sermonQuery).then( (data: sermonType) => {
-        console.log(data.content);/**/
-        //this.content = data.content.rendered;
+      this.wpService.getSermon(sermonQuery).then((data: sermonType) => {
+        //console.log(data.content);/**/
+        this.content = data.content.rendered;
       });/**/
       let mediaQuery = "?parent=" + this.sermonId;
-      this.wpService.getMedia(mediaQuery).then((mediaData) => {
+      this.wpService.getMedia(mediaQuery).then((mediaData: mediaType) => {
         //console.log(mediaData.source_url);
-        //this.media = mediaData.source_url;
-        //this.audio = '<video controls><source src={{media?.source_url}} type="video/mp4"></video>';
+        this.media = mediaData.source_url;
+        this.audio = '<video controls><source src={{media?.source_url}} type="video/mp4"></video>';
       });
     }
   }
