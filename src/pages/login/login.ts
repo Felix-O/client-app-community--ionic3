67b5f2153300bfb87156374561c8037e099371b4
Patrick Odum <felix.oje@gmail.com>
2018-03-19 01:19:10 -0700

    :/

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -44,7 +44,6 @@ export class LoginPage {
 
     ionViewDidEnter(){
       this.googlePopup().then(res => {
-        this.user = this.afAuth.authState;
         this.m1 = this.user;
         this.showAlert(this.m1, this.m2, this.m3);
       });
