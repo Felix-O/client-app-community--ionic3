4bd0f3f62fc4c6fd53e4de8db5a83b6c0b853ced
Patrick Odum <felix.oje@gmail.com>
2018-03-20 01:55:58 -0700

    :/

--- server.js
+++ server.js
@@ -7,12 +7,16 @@ var cors = require('cors');
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({'extended':'true'}));
 app.use(bodyParser.json());
-app.use(cors()); 
+app.use(cors());
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  if(req.url === "/google9fc4e97098695a2e.html"){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.createReadStream(__dirname + '/google9fc4e97098695a2e.html', 'utf8').pipe(res);
+  }
   next();
 });
 
